05a2d3fd316dbba960c2f1f0a22a32ab45749f80
juan <pardojuanmanuel99@gmail.com>
2019-03-26 14:26:35 -0800

    Se volvieron a borrar los comentarios

--- probandogithub/ProbandoGitHub.java
+++ probandogithub/ProbandoGitHub.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package probandogithub;
 
-/**
- *
- * @author juanm
- */
 public class ProbandoGitHub {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ahora hice una modificacion en el archivo");
     }
     
 }
